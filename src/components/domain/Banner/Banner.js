a02e5f53e9856e673d86143eace2c27ec4141bae
Jeongs <kimm.jeongs@gmail.com>
2022-02-22 14:53:39 +0900

    :dizzy:  UI: hover 애니메이션 적용 In Banner

--- src/components/domain/Banner/Banner.js
+++ src/components/domain/Banner/Banner.js
@@ -7,6 +7,7 @@ export default class Banner extends Component {
         {
           id: 1,
           src: './images/banner1.png',
+          link: 'https://kukka.kr/brand/story/',
           direction: 'left',
           title: '꾸까 브랜드 이야기',
           desc: '꽃으로 라이프스타일을 만들어가는 <br /> kukka의 문화를 소개해요.',
@@ -15,6 +16,7 @@ export default class Banner extends Component {
         {
           id: 2,
           src: './images/banner2.png',
+          link: 'https://kukka.kr/brand/showroom/',
           direction: 'right',
           title: '꾸까 오프라인 쇼룸',
           desc: '꽃을 가까이서 만져보고 향기도 맡아보고, <br /> 꽃 속에서 작은 휴식을 누릴 수 있는 kukka의 오프라인 쇼룸',
@@ -29,12 +31,12 @@ export default class Banner extends Component {
 
     return `
 		<div class="container-fluid">
-			<ul class="row">
+			<ul class="row banner-list">
 				${items
           .map(
-            ({ id, src, direction, title, desc, more }) => `
+            ({ id, src, link, direction, title, desc, more }) => `
 					<li class="col-md-6 banner-item" id='${id}'>
-					<a href="">
+					<a href=${link} class="banner-link">
 					<img class="banner-img" src="${src}" alt="" aria-hidden/>
 					<div class="info ${direction}">
 						<h2 class="banner-title">${title}</h2>
